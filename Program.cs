49c2bf25080f8152f100db57387bd2b7a5eda72f
Roman Klimenko <romaklimenko@gmail.com>
2015-10-10 20:27:51 +0200

    dnx run works!

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Test
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Hello from DNX!");
+		}
+	}
+}
