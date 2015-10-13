7dcb8e5ff90b2e0eeb798c6a57458882b83bb48e
Roman Klimenko <romaklimenko@gmail.com>
2015-10-13 20:13:57 +0200

    test command

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,6 +6,11 @@ namespace Test
 	{
 		static void Main(string[] args)
 		{
+			Console.ForegroundColor = ConsoleColor.Blue;
+			foreach (var arg in args)
+			{
+				Console.WriteLine(arg);
+			}
 			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("Hello from DNX!");
 		}
